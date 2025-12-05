3c68120aad71fafdf8b650ad22025d68c619842b
klimb <manulc0rparation@gmail.com>
2025-12-05 11:17:26 +0400

    init

---
+++ src/main/java/com/klim/birukou/book_hub/BookHubApplication.java
@@ -0,0 +1,13 @@
+package com.klim.birukou.book_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookHubApplication.class, args);
+	}
+
+}
